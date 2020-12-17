f903c02e33b80c53407d6ee8bc006619aefc3cc0
Fede <chiellofederico@gmail.com>
2020-12-17 15:20:16 +0100

    prompt + array

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
 // Creare una lista di cognomi
 
-// Istruzioni
-// 1. Chiedere all’utente il cognome;
-// 2. Inserirlo in un array con altri cognomi;
-// 3. Stampare la lista ordinata alfabeticamente;
-// 4. Scrivere la posizione "umana" (quindi per come contiamo noi, non l'indice dell'array) della lista in cui il nuovo utente si trova.
+// Istruzioni:
+// (1) Chiedere all’utente il cognome;
+// (2) Inserirlo in un array con altri cognomi;
+// (3) Stampare la lista ordinata alfabeticamente;
+// (4) Scrivere la posizione "umana" (quindi per come contiamo noi, non l'indice dell'array) della lista in cui il nuovo utente si trova.
+
+
+// (1) Chiedere cognome.
+var cognomeUtente = prompt('Inserisci il tuo cognome:');
+
+// (2) Creare array con altri cognomi ed inserire quello dell'utente.
+var cognomi = ['Rossi', 'Immobile', 'Ronaldo', 'Maldini'];
+
+cognomi.push(cognomeUtente);
+
+console.log(cognomi);
