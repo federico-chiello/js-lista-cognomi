3fd4347ac6c692903a206717b297359cfc61f88e
Fede <chiellofederico@gmail.com>
2020-12-17 15:10:29 +0100

    Istruzioni esercizio

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-alert('Ciao')
+// Creare una lista di cognomi
+
+// Istruzioni
+// 1. Chiedere all’utente il cognome;
+// 2. Inserirlo in un array con altri cognomi;
+// 3. Stampare la lista ordinata alfabeticamente;
+// 4. Scrivere la posizione "umana" (quindi per come contiamo noi, non l'indice dell'array) della lista in cui il nuovo utente si trova.
